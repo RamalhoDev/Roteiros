a65b869ce8d5482a231122a40f403d09a1a9f931
SAndradeTC <33987228+SAndradeTC@users.noreply.github.com>
2018-05-23 13:58:51 -0300

    Commit

--- Projeto_2/includes/GerenteDePersistencia.h
+++ Projeto_2/includes/GerenteDePersistencia.h
@@ -4,10 +4,14 @@
 #include "/home/rcr/Documentos/Backup_Mint/GITS/Roteiros/Projeto_2/includes/Imovel.h"
 
 class GerenteDePersistencia{
+    
+    //Escreve no arquivo
     public:
         GerenteDePersistencia(){}
         ~GerenteDePersistencia(){}
         list <Imovel> recuperaListaImoveis();
+    
+        //Salva a lista após alguma modificação
         void salvaListaImoveis(Imovel *imovel);
 
 };
