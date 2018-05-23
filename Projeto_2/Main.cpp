cc14547c40f79b44bac69e3b7fa9dc8b84d6ebec
SAndradeTC <33987228+SAndradeTC@users.noreply.github.com>
2018-05-23 13:34:55 -0300

    Comment

--- Projeto_2/Main.cpp
+++ Projeto_2/Main.cpp
@@ -44,7 +44,7 @@ int main(void){
 		cout <<"\t\t\tMENU\n";
 		cout<< "----------------------------------------------\n\n";
 
-
+		//Menu de acesso, ocorre a escolha da ação desejada
 		cout << "Escolha a operação que deseja realizar:\n";
 		cout << "\t1 - Consultar Imoveis\n";
 		cout << "\t2 - Cadastar Imoveis\n";
@@ -54,9 +54,12 @@ int main(void){
 		getchar();
 
 		if(menu == 1){
+			//Consulta os imóveis
 			list<Imovel> imoveis;
 
 			cout<<"Quais imoveis gostaria de consultar?\n";
+			
+			//Especifica o tipo de imóvel que o cliente procura
 			
 			cout<<"\t1 - Imoveis para Alugar por Bairro\n" << 
 				  "\t2 - Imovel para Vender por Bairro\n" << 
@@ -71,85 +74,160 @@ int main(void){
 			getchar();
 
 			if(tipodeConsulta == 1){
+				//Consulta de imoveis para ALUGAR por bairro
 			
 				cout << "\t\tInsira o bairro desejado: ";
+				
+				//Ler o nome do bairro
 				getline(cin,auxiliar);
+				
+				//Procura dentre todos os imóveis para ALUGAR, os que possuem o  bairro desejado
 				imoveis =imobiliaria->getImoveisParaAlugarPorBairro(auxiliar);
+				
+				//Imprime todos os Imoveis de acordo com a busca por bairro
 				ImprimirDescricao(imoveis);
 
-			}else if(tipodeConsulta == 2){
-			
+			}else if(tipodeConsulta == 2);
+				//Consulta de imoveis para VENDER por bairro
+				
 				cout << "\t\tInsira o bairro desejado: ";
+				//Ler o nome do bairro
 				getline(cin,auxiliar);
+				
+				//Procura dentre todos os imóveis para VENDER, os que possuem o  bairro desejado
 				imoveis = imobiliaria->getImoveisParaVenderPorBairro(auxiliar);
+				
+				//Imprime todos os Imoveis de acordo com a busca por bairro
 				ImprimirDescricao(imoveis);
 
 			}else if(tipodeConsulta == 3){
+				//Consulta por Cidade
 			
 				cout << "\t\tInsira a cidade desejada: ";
+				//Ler o nome da Cidade
 				getline(cin,auxiliar);
-				imoveis = imobiliaria->getImoveisPorCidade(auxiliar);	
+			
+				//Procura dentre todos os imóveis do sistema, os que estejam na cidade desejada
+				imoveis = imobiliaria->getImoveisPorCidade(auxiliar);
+			
+				//Imprime todos os Imoveis de acordo com a busca
 				ImprimirDescricao(imoveis);
 
 			}else if(tipodeConsulta == 4){
-
+				//Imoveis por Tipo
+				//Permite o cliente escolher o tipo de imovel que deseja procurar
 				tipoDeImovel = Selecao();
+				
+				//Pesquisa o imovel a procura de acordo com  a base cdastrada no sistema
 				imoveis = imobiliaria->getImoveisPorTipo(tipoDeImovel);
+			
+				//Imprime os imoveis de acordo com o tipo de consulta escolhida
 				ImprimirDescricao(imoveis);
 			
 			}else if(tipodeConsulta == 5){
-
+				//Consulta todos os imoveis
+			
+				//Acessa todos os imoveis cadastrados
 				imoveis = imobiliaria->getImoveis();
+			
+				//Exibe a base de dados
 				ImprimirDescricao(imoveis);
 
 			}else if(tipodeConsulta == 6){
+				//Aluguel por tipo de imóvel
 				
+				//Seleciona o tipo de imovel
 				tipoDeImovel = Selecao();
+			
+				//Pesquisa todos os imoveis para alugar de acordo com o tipo inserido
 				imoveis = imobiliaria->getImoveisPorTipoParaAlugar(tipoDeImovel);
+			
+				//Imprime os resultados da busca
 				ImprimirDescricao(imoveis);
 			}
 
 		}else if(menu == 2){
-
+			//Cadastrar imoveis
 			GerenteDePersistencia *escrever;
 			tipoDeImovel = Selecao();
 
 			if(tipoDeImovel == 1){
-
+				
+				//Cadastra uma Casa
 				Casa *casa;
+				
+				//Acessa a Lista para cadastrar o Imovel
 				casa = imobiliaria->cadastrarCasa(imobiliaria->cadastrarEndereco(), tipoDeImovel);
+				
+				//Salva o imovel cadastrado
 				escrever->salvaListaImoveis(casa);
+				
+				//Exclui o objeto
 				casa->~Casa();
 			
 			}else if(tipoDeImovel == 2){
-
+				
+				//Cadastra um Apartamento
 				Apartamento *apartamento;
+				
+				//Acessa a Lista para cadastrar o Imovel
 				apartamento = imobiliaria->cadastrarApartamento(imobiliaria->cadastrarEndereco(), tipoDeImovel);
+				
+				//Salva o imovel cadastrado
 				escrever->salvaListaImoveis(apartamento);
+				
+				//Exclui o objeto
 				apartamento->~Apartamento();
 			
 			}else if(tipoDeImovel==3){
+				
+				//Cadastra um terreno
 				Terreno *terreno;
+				
+				//Acessa a Lista para cadastrar o Imovel
 				terreno = imobiliaria->cadastrarTerreno(imobiliaria->cadastrarEndereco(), tipoDeImovel);
+				
+				//Salva o imovel cadastrado
 				escrever->salvaListaImoveis(terreno);
+				
+				//Exclui o objeto
 				terreno->~Terreno();
 			}
 			else if(tipoDeImovel == 4){
-
+				//Cadastra um Flat
 				Flat *flat;
+				
+				//Acessa a Lista para cadastrar o Imovel
 				flat = imobiliaria->cadastrarFlat(imobiliaria->cadastrarApartamento(imobiliaria->cadastrarEndereco(), tipoDeImovel));
+				
+				//Salva o imovel cadastrado
 				escrever->salvaListaImoveis(flat);
+				
+				//Exclui o objeto
 				flat->~Flat();
 			
 			}else if(tipoDeImovel == 5){
+				
+				//Cadastra um Studio
 				Studio *studio;
+				
+				//Acessa a Lista para cadastrar o Imovel
 				studio = imobiliaria->cadastrarStudio(imobiliaria->cadastrarFlat(imobiliaria->cadastrarApartamento(imobiliaria->cadastrarEndereco(), tipoDeImovel)));
+				
+				//Salva o imovel cadastrado
 				escrever->salvaListaImoveis(studio);
+				
+				//Exclui o objeto
 				studio->~Studio();
 			}
 
 			escrever->~GerenteDePersistencia();
+			//Exclui o objeto
+		
 		}else if(menu == 3){
+			//Finaliza o Programa
+			
+			//Desepede-se do usuário
 			cout << "Obrigado por utilizar o programa !" << endl;
 			break;
 		}
